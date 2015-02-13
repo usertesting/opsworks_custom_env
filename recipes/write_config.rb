f667825719f71fcc6c59297aadd1f7be0709ad84
Claude Vessaz <claude.vessaz@safe.com>
2015-02-12 19:27:50 -0800

    Modify to use with OpsWorks app ENV vars

--- recipes/write_config.rb
+++ recipes/write_config.rb
@@ -6,7 +6,11 @@ node[:deploy].each do |application, deploy|
   custom_env_template do
     application application
     deploy deploy
-    env node[:custom_env][application]
+    env deploy[:environment_variables]
+  end
+
+  link "#{deploy[:deploy_to]}/shared/config/application.yml" do
+    to "#{deploy[:deploy_to]}/current/config/application.yml"
   end
   
 end

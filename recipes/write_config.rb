3e90e1219b6400a2cc1d9e9f336de8183d8c10fe
Bob Ziuchkovski <bob.ziuchkovski@gmail.com>
2015-05-29 17:37:00 -0600

    Don't create the application.yml symlink

--- recipes/write_config.rb
+++ recipes/write_config.rb
@@ -2,15 +2,11 @@
 # See https://forums.aws.amazon.com/thread.jspa?threadID=118107
 
 node[:deploy].each do |application, deploy|
-  
+
   custom_env_template do
     application application
     deploy deploy
     env deploy[:environment_variables]
-  end
-
-  link "#{deploy[:deploy_to]}/current/config/application.yml" do
-    to "#{deploy[:deploy_to]}/shared/config/application.yml"
   end
   
 end
